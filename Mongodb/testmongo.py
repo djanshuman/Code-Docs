ba9af70e352358e96827a886611eb43c85ef0be1
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2018-07-24 16:39:32 +0530

    Update testmongo.py

--- Mongodb/testmongo.py
+++ Mongodb/testmongo.py
@@ -1,18 +1,18 @@
 # Python Code to fetch JSON format data through API call and inserting to MongoDB database ***
 # We have created database= mydatabase and collections=Customers ***
 
-
+#Importing library functions
 import requests
-from pprint import pprint
-response = requests.get("https://api.data.gov.in/resource/31f36e27-498d-4dd6-ae83-37bb1f8f5383?api-key=579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b&format=json&offset=0&limit=50")
 import json
 import pymongo
+from pprint import pprint
 from pymongo import MongoClient
+#Connecting to API and storing the response
+response = requests.get("https://api.data.gov.in/resource/31f36e27-498d-4dd6-ae83-37bb1f8f5383?api-key=579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b&format=json&offset=0&limit=50")
 client = MongoClient('localhost', 27017)
 db=client.mydatabase
 mycol=db.Customers
 x=response.content
-
 #Convert received string to JSON object of dict type
 import json
 json_string=x
