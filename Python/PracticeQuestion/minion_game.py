9b1ed59b5267d37fff3bbffa2a0c9bad41532a27
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2018-09-19 02:18:58 +0530

    Create minion_game.py
    The Minion Game Kevin and Stuart want to play the 'The Minion Game'. Game Rules
    Both players are given the same string, .
    Both players have to make substrings using the letters of the string . Stuart has to make words starting with consonants.
    Kevin has to make words starting with vowels.
    The game ends when both players have made all possible substrings.
    Scoring
    A player gets +1 point for each occurrence of the substring in the string .
    For Example:
    String = BANANA
    Kevin's vowel beginning word = ANA
    Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
    
    Your task is to determine the winner of the game and their score.
    Input Format
    A single line of input containing the string .
    Note: The string will contain only uppercase letters: .
    
    Output Format
    Print one line: the name of the winner and their score separated by a space. If the game is a draw, print Draw.
    Sample Input
     BANANA
    Sample Output
    Stuart 12
    Note :
    Vowels are only defined as
    . In this problem,
    is not considered a vowel.

---
+++ Python/PracticeQuestion/minion_game.py
@@ -0,0 +1,50 @@
+import cProfile
+
+def minion_game_1(string):
+    # your code goes here
+    vowels = 'AEIOU'
+    ln=len(string)
+    kevin_score = 0
+    stuart_score = 0
+    for i in range(ln):
+        if s[i] in vowels:
+            kevin_score += (ln-i)
+        else:
+            stuart_score += (ln-i)
+
+    if kevin_score > stuart_score:
+        print ("Kevin", kevin_score)
+    elif kevin_score < stuart_score:
+        print ("Stuart", stuart_score)
+    else:
+        print ("Draw")
+
+
+def minion_game_2(string):
+    # your code goes here
+    vowels = 'AEIOU'
+    ln=len(string)
+    kevin_score = 0
+    stuart_score = 0
+    for i in range(len(string)):
+        if s[i] in vowels:
+            kevin_score += (len(string)-i)
+        else:
+            stuart_score += (len(string)-i)
+
+    if kevin_score > stuart_score:
+        print ("Kevin", kevin_score)
+    elif kevin_score < stuart_score:
+        print ("Stuart", stuart_score)
+    else:
+        print ("Draw")
+
+def ctest(s):
+    print('calling without len stats')
+    cProfile.runctx("minion_game_1(s)", {'minion_game_1': minion_game_1},{'s':s})
+    print('calling with len stats')
+    cProfile.runctx("minion_game_2(s)", {'minion_game_2': minion_game_2}, {'s': s})
+
+if __name__ == '__main__':
+    s = input()
+    ctest(s)
